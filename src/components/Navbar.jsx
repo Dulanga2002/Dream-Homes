0a620590194f865dbae4aa38217295f8be269370
DulangaNikeshala <165166019+Dulanga2002@users.noreply.github.com>
2025-09-23 18:25:23 +0530

    crate the show mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,13 @@ const Navbar = () => {
           
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full cursor-pointer'>Sign up</button>
-        <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
-        
+        <img onClick={()=> setShowMobileMenu(true)}src={assets.menu_icon} className='md:hidden w-7' alt="" />
+
       </div>
       {/*--------mobile-menu-------*/}
-      <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all '>
+      <div className={`md:hidden  ${showmobileMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
-          <img src={assets.cross_icon} className='w-6 ' alt="" />
+          <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className='w-6 ' alt="" />
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
           <a href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
